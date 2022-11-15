ba1ce4c60960321f2ac9038cf989f5c650e1306f
AliyaNurmakhanova <aliyajeon91@gmail.com>
2022-11-15 22:15:28 +0600

    updated

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def max2(a, b):
     if a > b:
         return a
     return b
-print(max2(2, -3))
+print("Max of this two numbers: ", max2(2, -3))
 
 # An example of calling a function inside a function.
 def factorial(x):
@@ -27,9 +27,17 @@ print("The factorial of", num, "is", factorial(num))
 def max3(a, b, c):
     return max2(a, max2(b, c))
 
-print(max3(2, 7, -3))
+print("Max of this three numbers: ", max3(2, 7, -3))
 
-# Create a function that returns a list, tuple, dictionary.
+# Create a function that returns a list,
+
+# tuple,
+
+# dictionary.
 
 
-# Give an example of using the Map, Filter, and Reduce functions.
+# Give an example of using the Map,
+
+# Filter,
+
+# and Reduce functions.
