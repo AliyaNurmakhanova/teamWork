d4520fcf1b2601b64e9244a7b7a7247e2aa52493
AliyaNurmakhanova <aliyajeon91@gmail.com>
2022-11-15 21:26:57 +0600

    example 1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#
+# Creating a functional and
+a = 0
+def increment():
+    global a
+    a += 1
+
+increment()
+print(a)
+
+# non-functional function.
+def increment2(x):
+    return x + 1
+print(increment2(3))
+
+# An example of calling a function inside a function.
+
+# Create a function that returns a list, tuple, dictionary.
+
+# Give an example of using the Map, Filter, and Reduce functions.
